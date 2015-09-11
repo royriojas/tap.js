7fda0c5a48e06424715a923e3dd3f9e43853b078
royriojas <roy@kno.com>
2015-09-10 20:39:29 -0500

    FEAT: Add taphold and swipe detection

--- tap.js
+++ tap.js
@@ -1,7 +1,21 @@
 var debounce = require( 'debouncy' );
+var extend = require( 'extend' );
+var eventHelper = require( 'dom-event-special' );
 
-function Tap( el ) {
+function Tap( el, opts ) {
   var me = this;
+
+  me._opts = {
+    minSwipeDeltaX: 25,
+    minSwipeDeltaY: 25,
+    taphold: true,
+    swipe: true,
+    minTapDisplacementTolerance: 10,
+    tapholdMinThreshold: 500,
+    swipeThreshold: 1000
+  };
+
+  extend( me._opts, opts );
 
   var ele = me.el = (typeof el === 'object' && el !== null) ? el : document.getElementById( el );
   me.moved = false; // flags if the finger has moved
@@ -51,26 +65,52 @@ tapProto.start = function ( e ) {
     ele.addEventListener( 'touchmove', me, false );
     ele.addEventListener( 'touchend', me, false );
     ele.addEventListener( 'touchcancel', me, false );
-
+    me.checkForTaphold();
     me.blockMouseEvents();
   }
 
   if ( e.type === 'mousedown' && me._mouseEventsAllowed ) {
     ele.addEventListener( 'mousemove', me, false );
     ele.addEventListener( 'mouseup', me, false );
+    me.checkForTaphold();
   }
 
   me.startTarget = e.target;
 
+  me.handlingStart = true;
+
   me.moved = false;
   me.startX = me._getClientX( e ); //e.type === 'touchstart' ? e.touches[ 0 ].clientX : e.clientX;
   me.startY = me._getClientY( e ); //e.type === 'touchstart' ? e.touches[ 0 ].clientY : e.clientY;
+
+};
+
+tapProto.checkForTaphold = function () {
+  var me = this;
+  clearTimeout( me.tapHoldInterval );
+
+  me.tapHoldInterval = setTimeout( function () {
+
+    if ( me.moved || !me.handlingStart || !me._opts.taphold ) {
+      return;
+    }
+
+    eventHelper.fire( me.startTarget, 'taphold', {
+      bubbles: true,
+      cancelable: true
+    } );
+  }, me._opts.tapholdMinThreshold );
 };
 
 tapProto.move = function ( e ) {
   var me = this;
+
+  me._moveX = me._getClientX( e );
+  me._moveY = me._getClientY( e );
+
+  var tolerance = me._opts.minTapDisplacementTolerance;
   //if finger moves more than 10px flag to cancel
-  if ( Math.abs( me._getClientX( e ) - this.startX ) > 10 || Math.abs( me._getClientY( e ) - this.startY ) > 10 ) {
+  if ( Math.abs( me._moveX - this.startX ) > tolerance || Math.abs( me._moveY - this.startY ) > tolerance ) {
     this.moved = true;
   }
 };
@@ -86,17 +126,15 @@ tapProto.end = function ( e ) {
   ele.removeEventListener( 'mouseup', me, false );
 
   var target = e.target;
+  var endTime = Date.now();
+  var timeDelta = endTime - me.startTime;
+
+  me.handlingStart = false;
+  clearTimeout( me.tapHoldInterval );
 
   if ( !me.moved ) {
-    var endTime = Date.now();
 
-    var eventHelper = require( 'dom-event-special' );
-    if ( endTime - me.startTime > 500 ) { // more than 500ms between start and end and we ignore the event
-      me.startTime = null;
-      return;
-    }
-
-    if ( target !== me.startTarget ) { // if the target is not the same as the starting one
+    if ( target !== me.startTarget || timeDelta > me._opts.tapholdMinThreshold ) {
       me.startTarget = null;
       return;
     }
@@ -106,12 +144,62 @@ tapProto.end = function ( e ) {
       cancelable: true
     } );
 
+    return;
   }
+
+  if ( !me._opts.swipe || timeDelta > me._opts.swipeThreshold ) {
+    return;
+  }
+
+  var deltaX = me._moveX - me.startX;
+  var deltaY = me._moveY - me.startY;
+
+  var absDeltaX = Math.abs( deltaX );
+  var absDeltaY = Math.abs( deltaY );
+
+  var swipeInX = absDeltaX > me._opts.minSwipeDeltaX;
+  var swipeInY = absDeltaY > me._opts.minSwipeDeltaY;
+
+  var swipeHappen = swipeInX || swipeInY;
+
+  if ( !swipeHappen ) {
+    return;
+  }
+
+  var direction = '';
+
+  if ( absDeltaX >= absDeltaY ) {
+    direction += (deltaX > 0 ? 'right' : 'left');
+  } else {
+    direction += (deltaY > 0 ? 'down' : 'up');
+  }
+
+  eventHelper.fire( target, 'swipe', {
+    bubbles: true,
+    cancelable: true,
+    detail: {
+      direction: direction,
+      deltaX: deltaX,
+      deltaY: deltaY
+    }
+  } );
+
+  eventHelper.fire( target, 'swipe:' + direction, {
+    bubbles: true,
+    cancelable: true,
+    detail: {
+      direction: direction,
+      deltaX: deltaX,
+      deltaY: deltaY
+    }
+  } );
 };
 
 tapProto.cancel = function () {
   var me = this;
+  clearTimeout( me.tapHoldInterval );
 
+  me.handlingStart = false;
   me.moved = false;
   me.startX = 0;
   me.startY = 0;
@@ -122,6 +210,8 @@ tapProto.destroy = function () {
   var ele = me.el;
 
   me.handlingStart = false;
+  clearTimeout( me.tapHoldInterval );
+
   ele.removeEventListener( 'touchstart', me, false );
   ele.removeEventListener( 'touchmove', me, false );
   ele.removeEventListener( 'touchend', me, false );
